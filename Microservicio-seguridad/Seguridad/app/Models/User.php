b81e962a35a2616e7b540cfd17c25d1a63b802b8
Santipala1908 <sapalaciov@unal.edu.co>
2025-09-28 20:54:54 -0500

    Avances seguridad

--- Microservicio-seguridad/Seguridad/app/Models/User.php
+++ Microservicio-seguridad/Seguridad/app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relación con roles (muchos a muchos)
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_roles');
+    }
 }
