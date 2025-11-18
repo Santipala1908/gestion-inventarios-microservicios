986354f0138dc0ec308e58a081d288113df4a035
Santiago Palacio <sapalaciov@unal.edu.co>
2025-11-17 19:24:58 -0500

    Docker reportes

--- Reportes/app.py
+++ Reportes/app.py
@@ -126,4 +126,8 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.getenv("PORT", 8004)))
+   app.run(
+        debug=True,
+        host="0.0.0.0",                 
+        port=int(os.getenv("PORT", 8004))
+    )
